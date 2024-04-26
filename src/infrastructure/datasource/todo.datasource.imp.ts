cd3571c251da9a369d74397a746fc25531600965
PanchonSoto <panchon.ues@gmail.com>
2024-04-26 12:57:58 -0700

    todo datasource/repository implementation

---
+++ src/infrastructure/datasource/todo.datasource.imp.ts
@@ -0,0 +1,41 @@
+import { prisma } from "../../data/postgres";
+import { CreateTodoDto, TodoDatasource, TodoEntity, UpdateTodoDto } from "../../domain";
+
+
+
+export class TodoDatasourceImp implements TodoDatasource{
+
+    async create(createTodoDto: CreateTodoDto): Promise<TodoEntity> {
+        const todo = await prisma.todo.create({
+            data: createTodoDto!
+        });
+        return TodoEntity.fromObj(todo);
+    }
+
+    async getAll(): Promise<TodoEntity[]> {
+        const todos = await prisma.todo.findMany();
+        return todos.map(todo=>TodoEntity.fromObj(todo));
+    }
+
+    async findById(id: number): Promise<TodoEntity> {
+        const todo = await prisma.todo.findFirst({ where: { id }});
+        if(!todo) throw `Todo with id:${id} not found`;
+        return TodoEntity.fromObj(todo);
+    }
+
+    async updateById(updateTodoDto: UpdateTodoDto): Promise<TodoEntity> {
+        await this.findById(updateTodoDto.id);
+        const todoUpdated = await prisma.todo.update({
+            where: { id: updateTodoDto.id },
+            data: updateTodoDto!.values
+        });
+        return TodoEntity.fromObj(todoUpdated);
+    }
+
+    async deleteById(id: number): Promise<TodoEntity> {
+        await this.findById(id);
+        const todoDeleted = await prisma.todo.delete({where:{id}});
+        return TodoEntity.fromObj(todoDeleted);
+    }
+
+}
