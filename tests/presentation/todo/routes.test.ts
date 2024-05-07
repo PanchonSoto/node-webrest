b5c38e26e00ff408fa35a7ee86bcd5d5183b0315
PanchonSoto <panchon.ues@gmail.com>
2024-05-07 13:47:37 -0700

    get endpoints test

--- tests/presentation/todo/routes.test.ts
+++ tests/presentation/todo/routes.test.ts
@@ -1,5 +1,6 @@
 import request  from 'supertest';
 import { testServer } from '../../test-server';
+import { prisma } from '../../../src/data/postgres';
 
 
 describe('todo route testing', () => { 
@@ -11,15 +12,57 @@ describe('todo route testing', () => {
     afterAll(()=>{
         testServer.close();
     });
+
+    beforeEach(async()=>{
+        await prisma.todo.deleteMany();
+    });
+
+    const todo1 = {text:'Hola mundo 1'};
+    const todo2 = {text:'Hola mundo2'};
     
 
-    test('should return TODOs api/todos ', async() => { 
+    test('should return TODOs api/todos ', async() => {
         
-        const res = await request(testServer.app)
+        await prisma.todo.createMany({
+            data: [todo1, todo2]
+        });
+
+        const { body } = await request(testServer.app)
             .get('/api/todos')
             .expect(200);
 
-        console.log(res.body);
+        expect(body).toBeInstanceOf(Array);
+        expect(body.length).toBe(2);
+        expect(body[0].text).toBe(todo1.text);
+        expect(body[1].text).toBe(todo2.text);
+        expect(body[0].completedAt).toBeNull();
+    });
+
+    test('should return TODO api/todos/:id', async()=>{
+
+        const todo = await prisma.todo.create({ data: todo1 });
+
+        const { body } = await request(testServer.app)
+        .get(`/api/todos/${todo.id}`)
+        .expect(200);
+        
+        expect(body).toEqual({
+            id: todo.id,
+            text: todo.text,
+            completedAt: todo.completedAt
+        });
+
+    });
+
+    test('should return a 404 notFound api/todos/:id', async()=>{
+    
+        const todoId = 999;
+        const { body } = await request(testServer.app)
+        .get(`/api/todos/${todoId}`)
+        .expect(400);
+
+        expect(body).toEqual({ error: 'Todo with id:999 not found' });
+
     });
 
 });
